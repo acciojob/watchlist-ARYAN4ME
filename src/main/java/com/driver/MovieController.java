82391fd1f852df76ee35fc80e172e6ebb458e067
Mohit Prajapati <skparjapati9045@gmail.com>
2023-03-29 20:16:44 +0530

    submit success

--- src/main/java/com/driver/MovieController.java
+++ src/main/java/com/driver/MovieController.java
@@ -1,5 +1,6 @@
 package com.driver;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,7 +12,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/movies")
 public class MovieController {
-    MovieService movieService = new MovieService();
+    @Autowired
+    MovieService movieService;
 
     @PostMapping("/add-movie")
     public ResponseEntity<String> addMovie(@RequestBody Movie movie){
